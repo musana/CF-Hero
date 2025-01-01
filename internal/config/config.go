2eabd548e7d65b3d400268bb465df705b754726a
musana <musana@users.noreply.github.com>
2025-01-02 00:45:49 +0300

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -20,23 +20,23 @@ func ParseOptions() *models.Options {
 	)
 
 	createGroup(flagSet, "print options", "PRINT OPTIONS",
-		flagSet.BoolVar(&options.CF, "cf", false, "Print domains behind of Cloudflare"),
-		flagSet.BoolVar(&options.NCF, "non-cf", false, "Print domains is not behind of Cloudflare"),
+		flagSet.BoolVar(&options.CF, "cf", false, "Print domains behind Cloudflare"),
+		flagSet.BoolVar(&options.NCF, "non-cf", false, "Print domains not behind Cloudflare"),
 	)
 
 	createGroup(flagSet, "sources", "SOURCES",
-		flagSet.BoolVar(&options.Censys, "censys", false, "Include Censys in scan"),
-		flagSet.BoolVar(&options.SecurityTrails, "securitytrails", false, "Include SecurityTrails historical DNS records in scan"),
-		flagSet.BoolVar(&options.Shodan, "shodan", false, "Include Shodan historical DNS records in scan"),
-		flagSet.StringVar(&options.DomainList, "dl", "", "Domain list for sub/domain scan"),
-		flagSet.StringVar(&options.TargetDomain, "td", "", "Target domain for sub/domain scan"),
+		flagSet.BoolVar(&options.Censys, "censys", false, "Include Censys in scanning"),
+		flagSet.BoolVar(&options.SecurityTrails, "securitytrails", false, "Include SecurityTrails historical DNS records in scanning"),
+		flagSet.BoolVar(&options.Shodan, "shodan", false, "Include Shodan historical DNS records in scanning"),
+		flagSet.StringVar(&options.DomainList, "dl", "", "Domain list for sub/domain scanning"),
+		flagSet.StringVar(&options.TargetDomain, "td", "", "Target domain for sub/domain scanning"),
 	)
 
 	createGroup(flagSet, "configuration", "CONFIGURATION",
 		flagSet.StringVar(&options.HTTPMethod, "hm", "GET", "HTTP method."),
 		flagSet.StringVar(&options.JA3, "ja3", "772,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,18-10-16-23-45-35-5-11-13-65281-0-51-43-17513-27,29-23-24,0", "JA3 String"),
 		flagSet.StringVar(&options.UserAgent, "ua", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/113.0", "HTTP User-Agent"),
-		flagSet.StringVar(&options.Proxy, "px", "", "HTTP proxy to use"),
+		flagSet.StringVar(&options.Proxy, "px", "", "HTTP proxy URL"),
 	)
 
 	_ = flagSet.Parse()
